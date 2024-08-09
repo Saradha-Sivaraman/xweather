231533ef3d87efde7cee407f7455294bb2765a61
Saradha-Sivaraman <ssaradha85@gmail.com>
2024-08-09 17:20:34 +0530

    first commit

---
+++ src/components/WeatherApp.js
@@ -0,0 +1,73 @@
+// src/Weather.js
+import React, { useState } from 'react';
+import axios from 'axios';
+import './Weather.css';
+
+const Weather = () => {
+  const [city, setCity] = useState('');
+  const [weather, setWeather] = useState(null);
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  //const apiKey = 'YOUR_API_KEY'; // Replace with your OpenWeatherMap API key
+  let url =`http://api.weatherapi.com/v1/current.json?key=7b9c8208bc4c40aa92482251240808&q=${city}&aqi=no`
+
+  const fetchWeather = async () => {
+    setLoading(true);
+    setError('');
+    try {
+      const response = await axios.get(url);
+      setWeather(response.data);
+    } catch (err) {
+      setError('City not found');
+      setWeather(null);
+      alert('Failed to fetch weather data'); // Alert for failed fetch
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetchWeather();
+  };
+
+  return (
+    <div className="weather-container">
+      <form  onSubmit={handleSubmit}>
+        <input 
+          type="text"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          placeholder="Enter city"
+        />
+        <button type="submit" className="searchButton">Search</button>
+      </form>
+      {loading && <p>Loading data…</p>}
+      {error && <p>{error}</p>}
+      {weather && (
+        <div className="weather-cards">
+          <div className="weather-card">
+            <h3>Temperature</h3>
+            <p>{weather.current.temp_c}&#8451;</p>
+          </div>
+          <div className="weather-card">
+            <h3>Humidity</h3>
+            <p>{weather.current.humidity} %</p>
+          </div>
+          <div className="weather-card">
+            <h3>Condition</h3>
+            <p>{weather.current.condition.text} </p>
+          </div>
+          <div className="weather-card">
+            <h3>Wind Speed</h3>
+            <p>{weather.current.wind_kph} kph</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Weather;
+
